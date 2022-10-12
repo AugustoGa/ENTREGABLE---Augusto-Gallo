50e0590078617b55bd070538761dc2370d6ca770
AugustoGallo <augustogallo98@gmail.com>
2022-10-12 14:38:23 -0300

    Entrega

---
+++ ProyectoFinal/routes/Carrito.router.js
@@ -0,0 +1,54 @@
+import express from "express";
+import Carrito from "../clases/Carrito.class.js";
+
+const router = express.Router();
+
+const carrito = new Carrito();
+
+router.post("/", async (req, res) => {
+	const carritoCreado = await carrito.addCarrito();
+	res.send(carritoCreado);
+});
+
+router.delete("/:id", async (req, res) => {
+	const carritoBorrado = await carrito.borrar(req.params.id);
+	res.send(carritoBorrado);
+});
+
+router.delete("/:id/productos/:idPrd", async (req, res) => {
+	const productoBorrado = await carrito.borrarProd(
+		req.params.idPrd,
+		req.params.id
+		);
+		res.send(productoBorrado);
+})
+
+router.get("/", (req, res) => {
+	carrito.listarAll().then(listaCarritos => {
+		res.send(listaCarritos);
+	})
+});
+
+router.get("/:id", async (req, res) => {
+	const carroBuscado = Number(req.params.id);
+	const cont = await carrito.listar(carroBuscado);
+	res.send(cont);
+});
+
+router.get("/:id/productos", async (req, res) => {
+	const carroBuscado = Number(req.params.id);
+	const cont = await carrito.listarProd(carroBuscado);
+	res.send(cont);
+	console.log(cont)
+});
+
+
+
+router.post("/:id/productos/:idPrd", async (req, res) => {
+	const respuesta = await carrito.guardarProductoEnCarrito(
+		req.params.idPrd,
+		req.params.id
+	);
+	res.send(respuesta);
+});
+export default router;
